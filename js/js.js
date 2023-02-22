0e06bd540aa17ebc96f4b69083ebdeff7b97f2c7
ivantxp <ivantxp@gmail.com>
2023-02-22 07:47:11 -0300

    21-02

--- js/js.js
+++ js/js.js
@@ -1,5 +1,9 @@
-let persona = document.getElementById("personaje")
-console.log(persona) 
+let personaje = document.getElementById("personaje")
+console.dir(personaje) 
 
 //buscar posivio en las porpiedades de personage
 
+let a = personaje.offsetTop 
+console.log(a)
+
+personaje.offsetTop - 50
