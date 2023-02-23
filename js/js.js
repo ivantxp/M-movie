5a9d1d0cfb394a478c8c39d6fe5f7ef47544e0e9
ivantxp <ivantxp@gmail.com>
2023-02-23 17:27:54 -0300

    23-02

--- js/js.js
+++ js/js.js
@@ -1,11 +1,10 @@
 let personaje = document.getElementById("personaje")
 
 personaje.onclick = salto
-
+personaje.onanimationend = salto
 function salto(){
     personaje.classList.toggle("salto")
     personaje.classList.toggle("corriendo")
-    setTimeout()
 }
 
 
