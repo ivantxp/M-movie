c486860c3ea849f50de64075873a38f8f1acffdb
Ivan Fuentes <ivantxp@gmail.com>
2023-02-24 11:13:34 -0300

    d

--- js/js.js
+++ js/js.js
@@ -7,9 +7,24 @@ function salto(){
     personaje.classList.toggle("corriendo")
 }
 
+let marciano = document.getElementById("marciano")
+
+function MarcianoMover(){
+    let b = 0
+    for( i = 0; i < 5000; i = i + 100){
+       
+        b = b + 1 
+        setTimeout(() => {
+            marciano.style.right =   b + "px"
+        }, i);  
+        console.log(b)
+        console.log(i)
 
 
+    }
+}
 
+MarcianoMover()
 
 //buscar posivio en las porpiedades de personage
 
